87e9182999f238f21e49bfa4b92b06268436aabd
Nimra370 <msdsf24m016@pucit.edu.pk>
2025-05-18 14:59:38 +0500

    Update test_preprocessing.py

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,10 +1,10 @@
 from src.data_preprocessing import load_and_preprocess
 
 def test_data_split():
-X_train, X_test, y_train, y_test = load_and_preprocess()
-assert len(X_train) > 0
-assert len(X_test) > 0
+    X_train, X_test, y_train, y_test = load_and_preprocess()
+    assert len(X_train) > 0
+    assert len(X_test) > 0
 
 def test_data_shapes():
-X_train, X_test, y_train, y_test = load_and_preprocess()
-assert X_train.shape[1] == 4
+    X_train, X_test, y_train, y_test = load_and_preprocess()
+    assert X_train.shape[1] == 4
